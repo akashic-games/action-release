49a5128695c855b6705ad01b87d099eda832df01
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2024-01-30 10:40:17 +0900

    Fix error when git commit

--- dist/index.js
+++ dist/index.js
@@ -56133,7 +56133,7 @@ const currentBranch = process.env.GITHUB_REF_NAME;
 		const version = packageJson["version"];
 		const git = simpleGit();
 
-		git
+		await git
 			.addConfig("user.name", inputs.gitName, undefined, "global")
 			.addConfig("user.email", inputs.gitEmail, undefined, "global");
 
